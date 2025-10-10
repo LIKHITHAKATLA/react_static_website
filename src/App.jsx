e93e339303637084e5aaa6d6c99c1d8cf0e8c9b9
LIKHITHAKATLA <likhithakatla1002@gmail.com>
2025-10-10 23:34:02 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,57 @@
 
-import "./App.css"
-import AboutUs from "./components/AboutUs/AboutUs"
-import ContactUs from "./components/ContactUs/ContactUs"
-import Footer from "./components/Footer/Footer"
-import Gallary from "./components/Gallary/Gallary"
-import Home from "./components/Home/Home"
-import Navbar from "./components/Navbar/Navbar"
-import Services from "./components/Services/Services"
-function App(){
+// import "./App.css"
+// import AboutUs from "./components/AboutUs/AboutUs"
+// import ContactUs from "./components/ContactUs/ContactUs"
+// import Footer from "./components/Footer/Footer"
+// import Gallary from "./components/Gallary/Gallary"
+// import Home from "./components/Home/Home"
+// import Navbar from "./components/Navbar/Navbar"
+// import Services from "./components/Services/Services"
+// function App(){
+//   return (
+//     <div>
+      
+//       <Navbar></Navbar>
+//       <Home></Home>
+//       <AboutUs></AboutUs>
+//       <Gallary></Gallary>
+//       <Services></Services>
+//       <ContactUs></ContactUs>
+//       <Footer></Footer>
+      
+//     </div>
+//   )
+
+// }
+
+// export default App
+
+
+
+
+
+
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import AboutUs from "./components/AboutUs/AboutUs";
+import Gallary from "./components/Gallary/Gallary";
+import Services from "./components/Services/Services";
+import ContactUs from "./components/ContactUs/ContactUs";
+import Footer from "./components/Footer/Footer";
+
+function App() {
   return (
     <div>
-      
-      <Navbar></Navbar>
-      <Home></Home>
-      <AboutUs></AboutUs>
-      <Gallary></Gallary>
-      <Services></Services>
-      <ContactUs></ContactUs>
-      <Footer></Footer>
-      
+      <Navbar />
+      <Home />
+      <AboutUs />
+      <Gallary />
+      <Services />
+      <ContactUs />
+      <Footer />
     </div>
-  )
-
+  );
 }
 
-export default App
-
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-// import {BrowserRouter} from "react-router-dom"
-import App from "./App.jsx"
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// // import {BrowserRouter} from "react-router-dom"
+// import App from "./App.jsx"
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     {/* <BrowserRouter> */}
+//     {/* <App></App> */}
+//  <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+//     {/* </BrowserRouter> */}
+
+//   </StrictMode>,
+// )
+
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // <-- make sure this is imported
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <BrowserRouter> */}
-    <App></App>
-
-    {/* </BrowserRouter> */}
-
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
